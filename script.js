2cc4c361ccf56d98714f33d467f62e8f06b54eb5
christophervallone22 <christophervallone22@gmail.com>
2025-08-03 18:16:12 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 //   https://script.google.com/macros/s/AKfycbyjoQiPqyo-UBKzzD6By5lgMrl6Cr4X8jtuWJXP9VlizvQshxS9NrijLFV7orcVOfTe/exec
+//   https://script.google.com/macros/library/d/1xLxlTSXuuLUvkJFDME81XPu__2hBpxXMIf1OcAckIJQ-oGjze4Krua6y/2
 document.getElementById("signup-form").addEventListener("submit", function (e) {
     e.preventDefault();
 
     let email = document.getElementById("email").value;
     let message = document.getElementById("message");
 
-    fetch("https://script.google.com/macros/s/AKfycbyjoQiPqyo-UBKzzD6By5lgMrl6Cr4X8jtuWJXP9VlizvQshxS9NrijLFV7orcVOfTe/exec", {
+    fetch("https://script.google.com/macros/library/d/1xLxlTSXuuLUvkJFDME81XPu__2hBpxXMIf1OcAckIJQ-oGjze4Krua6y/2", {
         method: "POST",
         mode: "no-cors",
         headers: { "Content-Type": "application/x-www-form-urlencoded" },
