c50f06114e5130251cfb85323236bb2e5221ee1e
christophervallone22 <christophervallone22@gmail.com>
2025-08-04 01:56:20 -0400

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ document.getElementById("signup-form").addEventListener("submit", function (e) {
     let email = document.getElementById("email").value;
     let message = document.getElementById("message");
 
-    fetch("https://script.google.com/macros/library/d/1xLxlTSXuuLUvkJFDME81XPu__2hBpxXMIf1OcAckIJQ-oGjze4Krua6y/2", {
+    fetch("https://script.google.com/macros/s/AKfycbyjoQiPqyo-UBKzzD6By5lgMrl6Cr4X8jtuWJXP9VlizvQshxS9NrijLFV7orcVOfTe/exec", {
         method: "POST",
         mode: "no-cors",
         headers: { "Content-Type": "application/x-www-form-urlencoded" },
